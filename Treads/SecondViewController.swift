a04d00148c19bddd5a113dcb566892d205625401
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-29 05:52:30 +0200

    Initial Commit

---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Mohamed Adel on 5/29/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
