6762f5a242a3e29578750eb09bdea8e25cd02ef1
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-30 05:53:03 +0200

    Begin Run VC UI Done

---
+++ Treads/Controller/BeginRunVC.swift
@@ -0,0 +1,31 @@
+//
+//  BeginRunVC.swift
+//  Treads
+//
+//  Created by Mohamed Adel on 5/29/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class BeginRunVC: UIViewController {
+
+    // MARK: Outlets
+    
+    // MARK: Properties
+    
+    // MARK: View Controller Life Cycle
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // MARK: Action
+    
+    
+    // MARK: Class Methods
+    
+    
+    // MARK: Self Defined Methods
+    
+}
