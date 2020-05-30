b68077f6f1931e9b5f6ee934ff7531ee3cdd205f
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-31 01:08:19 +0200

    create our Realm Object

---
+++ Treads/Model/Run.swift
@@ -0,0 +1,39 @@
+//
+//  Run.swift
+//  Treads
+//
+//  Created by Mohamed Adel on 5/31/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+      // create First Realm Object
+class Run: Object {
+     dynamic public private(set) var id = ""
+     dynamic public private(set) var date = NSDate()
+     dynamic public private(set) var pace = 0
+     dynamic public private(set) var distance = 0.0
+     dynamic public private(set) var duration = 0
+
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+    
+    override class func indexedProperties() -> [String] {
+        return ["pace", "date", "duration"]
+    }
+    
+    convenience init(pace: Int, distance: Double, duration: Int) {
+        self.init()
+        self.id = UUID().uuidString.lowercased()
+        self.date = NSDate()
+        self.pace = pace
+        self.distance = distance
+        self.duration = duration
+        
+    }
+    
+}
