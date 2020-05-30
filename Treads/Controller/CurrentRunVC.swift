787f93e2bb9de7622725ad03dc90c4dc6f9d4bc1
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-30 13:07:44 +0200

    Track Location (Runner location updating to Label)

--- Treads/Controller/CurrentRunVC.swift
+++ Treads/Controller/CurrentRunVC.swift
@@ -13,20 +13,47 @@ class CurrentRunVC: LocationVC {
     // MARK: Outlets
     @IBOutlet private weak var swipeBGImageView: UIImageView!
     @IBOutlet private weak var sliderImageView: UIImageView!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var paceLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var pauseBtn: UIButton!
+    
     // MARK: Properties
     
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var runDistance = 0.0
+    
     // MARK: View Controller Life Cycle
-   
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
-               sliderImageView.addGestureRecognizer(swipeGesture)
-               sliderImageView.isUserInteractionEnabled = true
-               swipeGesture.delegate = self as? UIGestureRecognizerDelegate
+        sliderImageView.addGestureRecognizer(swipeGesture)
+        sliderImageView.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.distanceFilter = 10
+        startRun()
+    }
+    func startRun() {
+        manager?.startUpdatingLocation()
+        
+    }
+    func endRun() {
+        manager?.stopUpdatingLocation()
+        
+    }
+    
     // MARK: Action
+    @IBAction func pauseBtnPressed(_ sender: UIButton) {
+        
+    }
     
     // MARK: Class Methods
     
@@ -56,4 +83,22 @@ class CurrentRunVC: LocationVC {
         }
     }
     
+}
+
+extension CurrentRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+        }
+    }
+     // Runner location updating to Label
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let location = locations.last {
+            runDistance += lastLocation.distance(from: location)
+            distanceLabel.text = "\(runDistance)"
+        }
+        lastLocation = locations.last
+    }
 }
