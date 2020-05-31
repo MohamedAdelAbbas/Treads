aa6164af8bc4d6c24302b4fa8d9c8b616023b913
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-31 07:47:11 +0200

    Add Realm Configuration (Migration to Fix Realm File)

--- Treads/Model/Run.swift
+++ Treads/Model/Run.swift
@@ -43,7 +43,7 @@ class Run: Object {
         REALM_QUEUE.sync {
             let run = Run(pace: pace, distance: distance, duration: duration, locations: locations)
             do {
-                let realm = try Realm()
+                let realm = try Realm(configuration: RealmConfig.runDataConfig)
                 try realm.write {
                     realm.add(run)
                     try realm.commitWrite()
@@ -57,7 +57,7 @@ class Run: Object {
     // to Fetech realm Object
     static func getAllRuns() -> Results<Run>? {
         do {
-            let realm = try Realm()
+            let realm = try Realm(configuration: RealmConfig.runDataConfig)
             var runs = realm.objects(Run.self)
             runs = runs.sorted(byKeyPath: "date", ascending: false)
             return runs
