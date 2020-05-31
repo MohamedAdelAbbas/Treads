6b5a5c71f3f72373c543fa0157a1e9debcc68f0d
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-31 05:25:39 +0200

    Add Set up UI Last Run View in BeginRunVC

--- Treads/Controller/BeginRunVC.swift
+++ Treads/Controller/BeginRunVC.swift
@@ -15,6 +15,12 @@ class BeginRunVC: LocationVC {
     // MARK: Outlets
     
     @IBOutlet weak var runMapView: MKMapView!
+    @IBOutlet weak var lastRunCloseBtn: UIButton!
+    @IBOutlet weak var paceLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var lastRunBGView: UIView!
+    @IBOutlet weak var lastRunStack: UIStackView!
     // MARK: Properties
     
     // MARK: View Controller Life Cycle
@@ -23,19 +29,38 @@ class BeginRunVC: LocationVC {
         super.viewDidLoad()
         checkLocationAuthStatus()
         runMapView.delegate = self
-        print("here are my Runs : \( Run.getAllRuns())")
+       // print("here are my Runs : \( Run.getAllRuns())")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.stopUpdatingLocation()
+        getLastRun()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
+    func getLastRun() {
+        guard let lastRun = Run.getAllRuns()?.first else {
+            lastRunStack.isHidden = true
+            lastRunBGView.isHidden = true
+            lastRunCloseBtn.isHidden = true
+            return }
+        lastRunStack.isHidden = false
+        lastRunBGView.isHidden = false
+        lastRunCloseBtn.isHidden = false
+        paceLabel.text = lastRun.pace.formatTimeDurationToString()
+        distanceLabel.text = "\(lastRun.distance.metersToMiles(places: 2))mi"
+        durationLabel.text = lastRun.duration.formatTimeDurationToString()
+    }
     
     // MARK: Action
+    @IBAction func lastRunCloseBtnPressed(_ sender: Any) {
+        lastRunStack.isHidden = true
+        lastRunBGView.isHidden = true
+        lastRunCloseBtn.isHidden = true
+    }
     
     @IBAction func locationCenterBtnPressed(_ sender: UIButton) {
     }
